417788cb86f226d18d8f6137c96f864605dbf25d
Esteban Alvarez <esteban.yesid2255@gmail.com>
2023-09-02 16:59:40 -0500

    primera entrega

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+const productos = [
+    { nombre: "Manzana", precio: 10 },
+    { nombre: "Pera", precio: 20 },
+    { nombre: "Mango", precio: 15 },
+    // Agrega más productos si es necesario
+  ];
+const productosSeleccionados = [];
+
+function agregarProductoSeleccionado(nombreProducto) {
+    const producto = productos.find((p) => p.nombre === nombreProducto);
+  
+    if (producto) {
+      productosSeleccionados.push(producto);
+    } else {
+      console.log("Producto no encontrado");
+    }
+  }
+
+function calcularCostoTotal() {
+    let costoTotal = 0;
+  
+    for (const producto of productosSeleccionados) {
+      costoTotal += producto.precio;
+    }
+  
+    return costoTotal;
+  }
+
+agregarProductoSeleccionado("Manzana");
+agregarProductoSeleccionado("Pera");
+agregarProductoSeleccionado("Mango");
+
+const costoTotal = calcularCostoTotal();
+console.log("Costo total de los productos seleccionados: $" + costoTotal);
